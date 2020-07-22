8e8a9e04aad30c423ecca9d590e45831edbaa4b4
Cigashamwa byamungu <ebyamungu28@gmail.com>
2020-07-22 15:28:47 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+P1 = int(input("Enter a number"))
+if P1 == 1:
+  print("They are carnivores.")
+elif P1 == 2:
+  print("They are found on every continent of the world except Antarctica.")
+elif P1 == 3:
+  print("They shed their skin a number of times a year in a process that usually lasts a few days.")
+elif P1 == 4:
+  print("They smell with their tongue.")
+
+else:
+      print( "They have internal ears but not external ones.")
